161098d4b1b2da1b263fd0cd7b5edab32d2deeab
elmerurbina <[elmerurbina570@gmail.com]>
2024-09-13 13:28:16 -0600

    Microservices Architecture design pattern structure added

--- urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),        # Admin site URL pattern
-    path('', include('general.urls')),       # Root URL pattern for general app
-]
