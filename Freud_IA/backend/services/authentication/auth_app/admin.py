d40ed47411c9b1b3d2b8d5106aef3ff8e6a3f481
elmerurbina <[elmerurbina570@gmail.com]>
2025-01-03 10:54:09 -0600

    models added

--- Freud_IA/backend/services/authentication/auth_app/admin.py
+++ Freud_IA/backend/services/authentication/auth_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from Freud_IA.backend.services.authentication.auth_app.migrations.models import User, Payment
+from migrations.models import User, Payment
 
 admin.site.register(User)
 admin.site.register(Payment)
