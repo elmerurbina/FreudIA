fb6198fe25d75492e7ee1c5268d559baeb3ea7d1
elmerurbina <[elmerurbina570@gmail.com]>
2025-01-03 11:47:22 -0600

    Django MS structure

--- Freud_IA/backend/utils/response.js
+++
@@ -1,9 +0,0 @@
-const generateResponse = (message, data = null, error = false) => {
-    return {
-        success: !error,
-        message,
-        data,
-    };
-};
-
-module.exports = { generateResponse };
