9e118a8d008ae8a802fe6ace0549358a4c2af3b7
elmerurbina <[elmerurbina570@gmail.com]>
2025-01-03 20:10:03 -0600

    urls for general app

--- Freud_IA/backend/services/general/config/urls.py
+++ Freud_IA/backend/services/general/config/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from Freud_IA.backend.services.general.general_app.migrations.views import RelaxingPlaceViewSet
+from Freud_IA.backend.services.general.general_app.migrations.views import RecommendedPlaceViewSet
 
+# Create a router and register the RecommendedPlace viewset
 router = DefaultRouter()
-router.register(r'places', RelaxingPlaceViewSet)
+router.register(r'recommended-places', RecommendedPlaceViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
 ]
